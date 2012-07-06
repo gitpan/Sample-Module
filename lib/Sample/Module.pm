5fde05149b7b63b9467bb1297ee3a6dfcfe43bd3
Abhishek Shende <abhshekisnot@gmail.com>
2012-07-06 15:24:13 +0000

    Import of ABHIISNOT/Sample-Module-0.1.0 from CPAN.
    gitpan-cpan-distribution: Sample-Module
    gitpan-cpan-version:      0.1.0
    gitpan-cpan-path:         ABHIISNOT/Sample-Module-0.1.0.tar.gz
    gitpan-cpan-author:       ABHIISNOT
    gitpan-cpan-maturity:     released

---
+++ lib/Sample/Module.pm
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+
+# ABSTRACT: A Sample module to play with DZ.
+package Sample::Module;
+{
+  $Sample::Module::VERSION = '0.1.0';
+}
+use English qw( -no_match_vars);
+use Carp;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Sample::Module - A Sample module to play with DZ.
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 AUTHOR
+
+Abhishek <abhishekisnot@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Abhishek.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
